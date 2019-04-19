5b9cc5be99987a1f61b3c2d2c5322931e2775be8
Alexandru Onaci <Alexandru.Onaci@iquestgroup.com>
2019-04-19 15:54:44 +0300

    add example for node GET request

--- node-request-example/app.js
+++ node-request-example/app.js
@@ -1,1 +1,18 @@
+const request = require('request');
+
 const USER_API = 'https://randomuser.me/api/';
+const BACON_API = 'https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1';
+
+const options = {
+    url: USER_API,
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8'
+    }
+};
+
+request(options, (err, res, body) => {
+    let jsonResponse = JSON.parse(body);
+    console.log(jsonResponse);
+});
