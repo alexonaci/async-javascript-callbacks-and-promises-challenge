934c13a400bd12442845e41d92f06aeb45871d92
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-04-16 13:07:27 +0300

    make it work

--- promise-exercise/app.js
+++ promise-exercise/app.js
@@ -1,30 +1,33 @@
-const orderApi = require('./lib/order.api');
-const customerApi = require('./lib/customer.api');
+const orderApi = require("./lib/order.api");
+const customerApi = require("./lib/customer.api");
 
 const submitOrder = (user) => {
+  let shoppingCart, zipCode, shippingRate, orderSuccessful;
 
-    let shoppingCart, zipCode, shippingRate, orderSuccessful;
+  // Get the current user's shopping cart
+  orderApi.getShoppingCartAsync(user).then((cart) => {
+    shoppingCart = cart;
+  });
 
-    // Get the current user's shopping cart
-    orderApi.getShoppingCartAsync(user).then((cart) => {
-        shoppingCart = cart;
-    });
+  // Also look up the ZIP code from their profile
+  customerApi.getProfileAsync(user).then((profile) => {
+    zipCode = profile.zipCode;
+  });
 
-    // Also look up the ZIP code from their profile
-    customerApi.getProfileAsync(user).then((profile) => {
-        zipCode = profile.zipCode;
-    });
-
+  setTimeout(function () {
     // Calculate the shipping fees
     shippingRate = orderApi.calculateShippingSync(shoppingCart, zipCode);
-
     // Submit the order
     orderApi.placeOrderAsync(shoppingCart, shippingRate).then((success) => {
-        orderSuccessful = success;
+      orderSuccessful = success;
     });
+  }, 3000);
 
-    console.log(`Your order ${orderSuccessful? "was" : "was NOT"} placed successfully`);
+  setTimeout(function () {
+    console.log(
+      `Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`
+    );
+  }, 5000);
 };
 
 submitOrder(12345);
-
--- promise-exercise/lib/order.api.js
+++ promise-exercise/lib/order.api.js
@@ -1,35 +1,38 @@
-const { shoppingData } = require('./shopping-data');
-const USER_ERROR_MESSAGE = 'Cannot find user, our super big db cluster only supports one entry and that one is 12345!';
+const { shoppingData } = require("./shopping-data");
+const USER_ERROR_MESSAGE =
+  "Cannot find user, our super big db cluster only supports one entry and that one is 12345!";
 
 const getShoppingCartAsync = (user) => {
-    return new Promise((resolve, reject) => {
-        if (user === shoppingData.profile.id) {
-            resolve(shoppingData.cart);
-        } else {
-            reject(USER_ERROR_MESSAGE);
-        }
-    })
+  return new Promise((resolve, reject) => {
+    if (user === shoppingData.profile.id) {
+      resolve(shoppingData.cart);
+    } else {
+      reject(USER_ERROR_MESSAGE);
+    }
+  });
 };
 
 const placeOrderAsync = (shoppingCart, shippingRate) => {
-    return new Promise((resolve, reject) => {
-        if (shoppingCart && shippingRate) {
-            resolve(true);
-        } else {
-            reject('Promise rejected! Missing shopping cart or shippingRate');
-        }
-    })
+  return new Promise((resolve, reject) => {
+    if (shoppingCart && shippingRate) {
+      resolve(true);
+    } else {
+      reject("Promise rejected! Missing shopping cart or shippingRate");
+    }
+  });
 };
 
 const calculateShippingSync = (shoppingCart, zipCode) => {
-    if (shoppingCart !== shoppingData.cart) {
-        throw new Error('Error in calculateShippingSync: Invalid cart');
-    }
-    if (zipCode !== shoppingData.profile.zipCode) {
-        throw new Error('Error in calculateShippingSync: Invalid zip code');
-    }
+  if (shoppingCart !== shoppingData.cart) {
+    throw new Error("Error in calculateShippingSync: Invalid cart");
+    // console.log(shoppingCart, "Invalid cart aici");
+  }
+  if (zipCode !== shoppingData.profile.zipCode) {
+    throw new Error("Error in calculateShippingSync: Invalid zip code");
+    // console.log(zipCode, "Invalid zipcode aici");
+  }
 
-    return 55.55;
+  return 55.55;
 };
 
 module.exports.getShoppingCartAsync = getShoppingCartAsync;
