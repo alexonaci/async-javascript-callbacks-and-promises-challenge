b3f672e78b57b67e7ceb5776d7ff8d0a51bcc119
Boda Alexandru - Florin <46644709+BodaAlex@users.noreply.github.com>
2020-04-16 15:28:32 +0300

    implement async/await functionality

--- promise-exercise/app.js
+++ promise-exercise/app.js
@@ -1,27 +1,30 @@
 const orderApi = require("./lib/order.api");
 const customerApi = require("./lib/customer.api");
 
-const submitOrder = (user) => {
+const submitOrder = async (user) => {
   let shoppingCart, zipCode, shippingRate, orderSuccessful;
 
   // Get the current user's shopping cart
-  orderApi.getShoppingCartAsync(user)
-    .then((cart) => { shoppingCart = cart; })
-    // Also look up the ZIP code from their profile
-    .then(customerApi.getProfileAsync(user)
-      .then((profile) => { zipCode = profile.zipCode; })
-    )
-    .then(() => {
-      // Calculate the shipping fees
-      shippingRate = orderApi.calculateShippingSync(shoppingCart, zipCode);
-      // Place order
-      orderApi.placeOrderAsync(shoppingCart, shippingRate)
-        .then((success) => {
-          orderSuccessful = success;
-          console.log(`Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`);
-        });
-    })
-    .catch((err) => { console.log(err); });
+  await orderApi.getShoppingCartAsync(user).then((cart) => {
+    shoppingCart = cart;
+  });
+
+  // Also look up the ZIP code from their profile
+  await customerApi.getProfileAsync(user).then((profile) => {
+    zipCode = profile.zipCode;
+  });
+
+  // Calculate the shipping fees
+  shippingRate = orderApi.calculateShippingSync(shoppingCart, zipCode);
+
+  // Place order
+  await orderApi.placeOrderAsync(shoppingCart, shippingRate).then((success) => {
+    orderSuccessful = success;
+  });
+
+  console.log(
+    `Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`
+  );
 };
 
 submitOrder(12345);
